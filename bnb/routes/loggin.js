7eb777b73c1064bcd05aaf8bcd2147a5f4c4fb73
mAlxc <alexcis@webe.fr>
2017-10-06 18:55:13 +0200

    PostLoggin start

--- bnb/routes/loggin.js
+++ bnb/routes/loggin.js
@@ -3,13 +3,22 @@ var router = express.Router();
 
 /* GET users listing. */
 router.post('/', function (req, res, next) {
-  //var user = req.params.user
- // var password = req.params.password
-  res.send('respond with a resource');
+  console.log(req.body)
+  if(!!req.body.name){
+    var user = req.body.name
+  }else{
+    res.status(402);
+  }
+  if(!!req.body.password){
+    var password = req.body.password
+  }else{
+    res.status(402)
+  }
+ loggin(user,password,res)
 });
 
-function loggin(user, password) {
-  console.log(user,password)
+function loggin(user, password,res) {
+  res.send([user,password])
 }
 
 
