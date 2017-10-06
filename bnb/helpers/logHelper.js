cd1cbd3e48a73533a4eeb45cc71d74d81caeba8a
mAlxc <alexcis@webe.fr>
2017-10-06 18:32:19 +0200

    commitation

--- bnb/helpers/logHelper.js
+++ bnb/helpers/logHelper.js
@@ -4,4 +4,8 @@ function logInfo(text) {
     console.log(text.bgWhite.black);
 }
 
+function logError(text) {
+    console.log(text.bgRed.blue);
+}
+
 exports.colors = colors;
--- bnb/routes/loggin.js
+++ bnb/routes/loggin.js
@@ -2,15 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.post('/', function(req, res, next) {
-    var user = req.params.user
+router.post('/', function (req, res, next) {
+  //var user = req.params.user
+ // var password = req.params.password
   res.send('respond with a resource');
 });
 
-function loggin(user,password){
-
-
-
+function loggin(user, password) {
+  console.log(user,password)
 }
+
 
 module.exports = router;
